3890e9c6ae62e77cd374503d8806c78e4922ce42
hthief <jrqf.matos@gmail.com>
2023-12-15 10:45:20 +0000

    Day 3 js first iteration

---
+++ 2023/day-3/day3.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const path = require("path");
+
+const part1 = (data) => {
+    const lines = data.split('\n');
+    let allPartsNumbers = [];
+
+    const isAdjacent = (symbolIndex, numberIndex, numberLength) => {
+        if (numberIndex <= symbolIndex - 1 && symbolIndex - 1 < numberIndex + numberLength) {
+            return true;
+        };
+        if (numberIndex <= symbolIndex && symbolIndex < numberIndex + numberLength) {
+            return true;
+        };
+        if (numberIndex <= symbolIndex + 1 && symbolIndex + 1 < numberIndex + numberLength) {
+            return true
+        };
+        return false;
+    }
+    const getNeighbouringNumbers = (lineIndex, symbolIndex) => {
+        const numbers = [];
+        for (let lineOffset = -1; lineOffset < 2; lineOffset++) {
+            if (lineIndex + lineOffset < 0 || lineIndex + lineOffset >= lines.length) {
+                continue;
+            }
+            const currentLine = lines[lineIndex + lineOffset];
+            const numbersInLine = currentLine.matchAll(/\d+/g);
+            for (const match of numbersInLine) {
+                const number = match[0];
+                if (isAdjacent(symbolIndex, match.index, number.length)) {
+                    numbers.push(parseInt(number));
+                }
+            }
+        }
+        return numbers;
+    }
+
+    for (let i = lines.length - 1; i >= 0; i--) {
+        const line = lines[i];
+        const symbolIndexes = line.split('').reduce((indexes, currentChar, currentIndex) => {
+            if (currentChar.match(/[^\.0-9]/)) {
+                indexes.push(currentIndex);
+            }
+            return indexes;
+        }, []);
+        for (const symbolIndex of symbolIndexes) {
+            allPartsNumbers = allPartsNumbers.concat(getNeighbouringNumbers(i, symbolIndex));
+        }
+
+    }
+
+    console.log(allPartsNumbers.reduce((sum, partNumber) => (sum + partNumber)));
+
+}
+
+
+fs.readFile(path.resolve(__dirname, "input"), 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    part1(data); //557705
+
+});
